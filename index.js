c230130d49775982c54cf7fdab8e19c36de13450
Alexandre Plateau <lexplt.dev@gmail.com>
2021-02-23 19:03:01 +0100

    updating index to fetch data using rcon

--- index.js
+++ index.js
@@ -7,22 +7,31 @@ const port = 3000;
 const config = require('./config');
 
 const Rcon = require('modern-rcon');
-const conn = new Rcon(config.rcon_ip, config.rcon_port, config.rcon_password);
+const conn = new Rcon(config.rcon_ip, config.rcon_port, config.rcon_password, 500 /* timeout */);
 
 app.use(express.static('public'));
 
 app.get('/info', async (req, res) => {
-    let co = await conn.connect();
-    let data = await co.send('/list');
-    console.log(data);
+    let online = "0", max_online = "0";
 
-    let reg = /There are (?<players>[0-9]{1,5}) of a max (?<max>[0-9]{1,5}) players? online/gm;
-    let match = reg.exec(data);
-    console.log(match.groups.players, match.groups.max);
+    try {
+        await conn.connect();
+        let data = await conn.send('/list');
+        let reg = /There are (?<players>[0-9]{1,5}) of a max of (?<max>[0-9]{1,5}) players? online/gm;
+        let match = reg.exec(data);
+        await conn.disconnect();
 
-    await co.disconnect();
+        online = match[1];
+        max_online = match[2];
+    } catch (err) {
+        online = "0";
+        max_online = "0";
+    }
 
-    res.send('hello world');
+    res.send({
+        "players_online": online,
+        "max_players": max_online,
+    });
 });
 
 app.listen(port, () => {
