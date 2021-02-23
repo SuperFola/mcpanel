459086994c6db7b9c035a570eb9410408a75f92b
Alexandre Plateau <lexplt.dev@gmail.com>
2021-02-24 00:04:30 +0100

    enhancing the front page

--- index.js
+++ index.js
@@ -7,30 +7,55 @@ const port = 3000;
 const config = require('./config');
 
 const Rcon = require('modern-rcon');
-const conn = new Rcon(config.rcon_ip, config.rcon_port, config.rcon_password, 500 /* timeout */);
+const conn = new Rcon(config.rcon_ip, config.rcon_port, config.rcon_password, 1000 /* timeout */);
 
 app.use(express.static('public'));
 
 app.get('/info', async (req, res) => {
     let online = "0", max_online = "0";
+    let difficulty = "Easy";
+    let seed = "0";
+    let border = "0";
+    let day = "0";
 
     try {
         await conn.connect();
-        let data = await conn.send('/list');
+        let data = await conn.send('list');
         let reg = /There are (?<players>[0-9]{1,5}) of a max of (?<max>[0-9]{1,5}) players? online/gm;
         let match = reg.exec(data);
-        await conn.disconnect();
-
         online = match[1];
         max_online = match[2];
+
+        console.log("difficulty")
+        difficulty = await conn.send('difficulty');
+        difficulty = /The difficulty is (?<diff>[A-Za-z]+)/gm.exec(difficulty)[1];
+
+        console.log("seed")
+        seed = await conn.send('seed')
+        seed = /Seed: \[(?<seed>[0-9]+)\]/gm.exec(seed)[1];
+
+        console.log("border")
+        border = await conn.send('worldborder get')
+        border = /The world border is currently (?<border>[0-9]+) blocks wide/gm.exec(border)[1];
+
+        console.log("day")
+        day = await conn.send('time query day')
+        day = /The time is (?<day>[0-9]+)/gm.exec(day)[1];
+
+        if (online !== "0")
+            await conn.send('say someone visited minecraft.12f.pl');
+        await conn.disconnect();
     } catch (err) {
-        online = "0";
-        max_online = "0";
+        console.log(err)
     }
 
     res.send({
         "players_online": online,
         "max_players": max_online,
+        "difficulty": difficulty,
+        "seed": seed,
+        "worldborder": border,
+        "day": day,
     });
 });
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,19 @@
 // currently-online
 
-function fetch_currently_online() {
+function fetch_and_update() {
     fetch('/info')
         .then(data => data.json())
         .then(json => {
             document.getElementById('currently-online').innerHTML = `${json['players_online']} out of ${json['max_players']}`;
+            document.getElementById('game-difficulty').innerHTML = json["difficulty"];
+            document.getElementById('seed').innerHTML = json["seed"];
+            document.getElementById('world-border').innerHTML = json["worldborder"];
+            document.getElementById('current-day').innerHTML = `The day is ${json['day']}`;
         });
 }
 
 document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
-        fetch_currently_online();
+        fetch_and_update();
     }
 };
