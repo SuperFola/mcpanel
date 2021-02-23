383a43c304c28d8003575661d7ed110eb33fd12e
Alexandre Plateau <lexplt.dev@gmail.com>
2021-02-23 19:09:34 +0100

    displaying server info on the index

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,15 @@
 // currently-online
-//
+
+function fetch_currently_online() {
+    fetch('/info')
+        .then(data => data.json())
+        .then(json => {
+            document.getElementById('currently-online').innerHTML = `${json['players_online']} out of ${json['max_players']}`;
+        });
+}
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        fetch_currently_online();
+    }
+};
