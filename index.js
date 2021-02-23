fa176e269163fc982a1a704280b827035819b555
Alexandre Plateau <lexplt.dev@gmail.com>
2021-02-23 15:31:50 +0100

    adding first code

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const config = require('./config');
+
+const Rcon = require('rcon');
+const conn = new Rcon(config.rcon_ip, config.rcon_port, config.rcon_password);
+
+conn.on('auth', () => console.log('Connected to RCON!'))
+    .on('response', (str) => console.log('Response: ', str))
+    .on('end', () => console.log('RCON closed'));
+conn.connect();
+
 app.use(express.static('public'));
 
-app.get('/hello', (req, res) => {
+app.get('/info', (req, res) => {
+    console.log(conn.send('/list'));
     res.send('hello world');
 });
 
